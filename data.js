dfc07220e5d845337517c5cb95f6ffcb2a18ff04
ibrrahimalkurdi <ibrraalkurdi0@gmail.com>
2025-09-10 01:33:23 +0300

    Create data.js
    Signed-off-by: ibrrahimalkurdi <ibrraalkurdi0@gmail.com>

---
+++ data.js
@@ -0,0 +1,10 @@
+let allData = {
+    teachers: {
+        "t1": { "id": "t1", "name": "مامۆستا تحسین إبراهیم دوسکی", "profileImage": "https://i.ibb.co/L95s4yv/IMG-1144.jpg", "bio": "مامۆستایێ لسەر سوونەتێ", "phone": "", "social": { "youtube": "", "telegram": "https://t.me/tahsin_doski", "facebook": "" } },
+        // ... هەتـا دوماهیێ
+    },
+    generalLectures: [
+        // ... هەتـا دوماهیێ
+    ],
+    // ... هەمی داتایێن تە یێن دی
+};
